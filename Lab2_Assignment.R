5d66d2a896beaaba4926f77abbe5c9a541b5f1b1
Ping Yuan <yuan.ping@my.utsa.edu>
2025-02-09 01:02:49 -0600

    read dataset

--- Lab2_Assignment.R
+++ Lab2_Assignment.R
@@ -9,22 +9,27 @@
 # 2. [60 points] Open the R file "Lab2_Assignment.R" and answer the questions
 
 #---- Q1. write the code to load the dataset "tract_covariates.csv" located under the "datasets" folder in your repository. Create an object called `opportunities` Use the data.table package to do this. ----
+library(data.table)
+opportunities<-fread("datasets/tract_covariates.csv")
 
+data.table::fread("datasets/tract_covariates.csv")#another method
 
 #---- Q2. On your browser, read and become familiar with the dataset metadata. Next write the code for the following:
 # Link to metadata: https://opportunityinsights.org/wp-content/uploads/2019/07/Codebook-for-Table-9.pdf 
 
+
 # what is the object class?
-
+class(opportunities)
 # how can I know the variable names?
-
+names(opportunities)
 # What is the unit of analysis? 
-
+View(opportunities)#the analysis unit is "tract"?
 # Use the `summary` function to describe the data. What is the variable that provides more interest to you?
-
+summary(opportunities)#hhinc_mean2000  interests me more
 # Create a new object called `sa_opportunities` that only contains the rows for the San Antonio area (hint: use the `czname` variable). 
-
-
+sa_opportunities<-subset(opportunities,czname=="San Antonio")
+sa_opportunities <- opportunities[opportunities$czname == "San Antonio", ]
+sa_opportunities
 # Create a plot that shows the ranking of the top 10 census tracts by Annualized job growth rate (`ann_avg_job_growth_2004_2013` variable) by census tract (tract variable). Save the resulting plot as a pdf with the name 'githubusername_p1.pdf' # Hint: for ordering you could use the `setorderv()` or reorder() functions, and the ggsave() function to export the plot to pdf. 
 
 # Create a plot that shows the relation between the `frac_coll_plus` and the `hhinc_mean2000` variables, what can you hypothesize from this relation? what is the causality direction? Save the resulting plot as a pdf with the name 'githubusername_p3.pdf'
